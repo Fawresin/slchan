5bc6c8c5c2b3b0ad01d891fa3a46a55136174c45
Fawresin <fawresin@gmail.com>
2020-01-11 01:14:05 -0400

    Testing an issue with cookies not being set on the production server.

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -136,8 +136,11 @@ class PostController extends BaseController {
                 $password_hash = password_hash($password, PASSWORD_DEFAULT);
             }
 
-            setcookie('slchan_name', $name, time() + COOKIE_EXPIRE_TIME, '/');
-            setcookie('slchan_pass', $password, time() + COOKIE_EXPIRE_TIME, '/');
+            if (!setcookie('slchan_name', $name, time() + COOKIE_EXPIRE_TIME, '/') or
+                !setcookie('slchan_pass', $password, time() + COOKIE_EXPIRE_TIME, '/'))
+                throw new Exception('Cookie was not set.');
+            $_COOKIE['slchan_name'] = $name;
+            $_COOKIE['slchan_pass'] = $password;
 
             $ip_address = $_SERVER['REMOTE_ADDR'];
             $ban_model = new BanModel();
